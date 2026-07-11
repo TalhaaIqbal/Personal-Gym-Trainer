ff4f2d4cda2f3e92ca38099cdf6de06de83d8dc9
Talha <talhaiqbal7272@gmail.com>
2026-07-12 01:08:16 +0500

    backblaze upload and download and test_b2_client

--- backend/app/routers/base.py
+++ backend/app/routers/base.py
@@ -4,6 +4,7 @@ from .auth_router import router as auth_router
 from .availability_router import router as availability_router
 from .booking_router import router as booking_router
 from .workout_plan_router import router as workout_plan_router
+from .file_router import router as file_router
 
 router = APIRouter()
 
@@ -12,3 +13,4 @@ router.include_router(auth_router)
 router.include_router(availability_router)
 router.include_router(booking_router)
 router.include_router(workout_plan_router)
+router.include_router(file_router)
--- backend/app/routers/file_router.py
+++ backend/app/routers/file_router.py
@@ -1,24 +1,29 @@
-from fastapi import APIRouter, HTTPException
-from services.b2_client import s3_client, BUCKET_NAME
+from fastapi import APIRouter, HTTPException, Depends
 import uuid
+import os
+from ..core.backblaze.b2_client import initiate_s3_client
+from ..core.middleware import get_current_trainer, get_current_user
 from ..schemas.file_schema import UploadRequest
 
 router = APIRouter(prefix="/videos", tags=["videos"])
 
+BUCKET_NAME = os.getenv("B2_BUCKET_NAME")
 
-@router.post("/presign-upload")
+
+@router.post("/presign-upload", dependencies=[Depends(get_current_trainer)])
 def get_presigned_upload_url(payload: UploadRequest):
     try:
         file_key = f"videos/{uuid.uuid4()}-{payload.filename}"
 
-        presigned_url = s3_client.generate_presigned_url(
+        client = initiate_s3_client(role="uploader")
+        presigned_url = client.generate_presigned_url(
             "put_object",
             Params={
                 "Bucket": BUCKET_NAME,
                 "Key": file_key,
                 "ContentType": payload.content_type,
             },
-            ExpiresIn=3600,  # 1 hour
+            ExpiresIn=3600,
         )
 
         return {
@@ -29,10 +34,11 @@ def get_presigned_upload_url(payload: UploadRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/presign-download/{file_key:path}")
+@router.get("/presign-download/{file_key:path}", dependencies=[Depends(get_current_user)])
 def get_presigned_download_url(file_key: str):
     try:
-        url = s3_client.generate_presigned_url(
+        client = initiate_s3_client(role="viewer")
+        url = client.generate_presigned_url(
             "get_object",
             Params={"Bucket": BUCKET_NAME, "Key": file_key},
             ExpiresIn=3600,
