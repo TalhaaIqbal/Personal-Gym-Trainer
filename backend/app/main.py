827a839e5bbae3387ea4512287daf6fa8f577e75
Talha <talhaiqbal7272@gmail.com>
2026-07-13 13:40:34 +0500

    Fix main file imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
-from app.routers import base
+from .routers import base
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from app.core.database import client
+from .core.database import client
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
